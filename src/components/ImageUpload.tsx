e5ad1d423803835ec1c36b7ac9134df4f4bf4abf
Andy Sen <andy.sen@icloud.com>
2023-11-04 20:58:11 -0400

    fixed s3 image upload issue

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -2,16 +2,29 @@
 import { useDropzone } from 'react-dropzone'
 import { Inbox } from 'lucide-react'
 import toast from 'react-hot-toast'
+import { getS3Url, uploadToS3 } from '@/lib/s3'
 
 const ImageUpload = () => {
 	const { getRootProps, getInputProps } = useDropzone({
 		maxFiles: 1,
 		accept: { 'image/*': ['.jpeg', '.jpg', '.png', '.webp'] },
-		onDrop: (acceptedFiles) => {
-			console.log(acceptedFiles)
-		},
-		onDropRejected: (fileRejections) => {
-			if (fileRejections[0]?.errors[0]?.code) toast.error(fileRejections[0].errors[0].code)
+		onDrop: async (acceptedFiles) => {
+			if (acceptedFiles.length === 0) {
+				toast.error('file type unsupported')
+				return
+			}
+			const file = acceptedFiles[0]
+			if (file.size > 8 *  1024 * 1024) {
+				toast.error('10mb limit')
+				return
+			}
+			try {
+				const data = await uploadToS3(file)
+				console.log('data', data)
+				console.log(getS3Url(data?.file_key!))
+			} catch (err) {
+				console.error(err)
+			}
 		}
 	})
   return (
--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,20 +1,28 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
-    region: process.env.NEXT_PUBLIC_S3_REGION
+    region: process.env.NEXT_PUBLIC_S3_REGION,
+    credentials: {
+        accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!
+    }
 })
 
 export const uploadToS3 = async (file: File) => {
-    const file_key = 'uploads/' + Date.now().toString() + file.name.replace(/[^a-zA-Z. ]/g, "").replace(/ /g, "_")
+    const file_key = 'uploads/' + Date.now().toString() + '-' + file.name.replace(/[^a-zA-Z0-9. ]/g, "").replace(/ /g, "_")
 
     const putObject = new PutObjectCommand({
         Bucket: process.env.NEXT_PUBLIC_S3_BUCKET,
         Key: file_key,
-        Body: file
+        ContentType: 'image/*',
+        Body: file,
     })
+
     try {
         const res = await s3Client.send(putObject)
         if (res.$metadata.httpStatusCode !== 200) throw Error('s3 upload error')
+
+        console.log('s3 status', res.$metadata.httpStatusCode);
 
         return Promise.resolve({
             file_key,
@@ -27,6 +35,6 @@ export const uploadToS3 = async (file: File) => {
 }
 
 export const getS3Url = (file_key:string) => {
-    const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET}.${process.env.NEXT_PUBLIC_S3_REGION}.amazonaws.com/${file_key}`
+    const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET}.s3.${process.env.NEXT_PUBLIC_S3_REGION}.amazonaws.com/${file_key}`
     return url
 }
